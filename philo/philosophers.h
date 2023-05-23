2adfa44af6ea724f9ca9f53fed93ad1d5d0213a3
Stepan Benes <sbenes@c1r6s4.42prague.com>
2023-05-23 16:49:43 +0200

    first files

---
+++ philo/philosophers.h
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sbenes <sbenes@student.42prague.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/23 16:36:04 by sbenes            #+#    #+#             */
+/*   Updated: 2023/05/23 16:48:59 by sbenes           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* You have to write a program for the mandatory part and another one for the bonus part
+(if you decide to do the bonus part). They both have to comply with the following rules:
+• Global variables are forbidden!
+• Your(s) program(s) should take the following arguments:
+number_of_philosophers time_to_die time_to_eat time_to_sleep
+[number_of_times_each_philosopher_must_eat]
+
+◦ number_of_philosophers: The number of philosophers and also the number
+of forks.
+◦ time_to_die (in milliseconds): If a philosopher didn’t start eating time_to_die
+milliseconds since the beginning of their last meal or the beginning of the simulation, they die.
+◦ time_to_eat (in milliseconds): The time it takes for a philosopher to eat.
+During that time, they will need to hold two forks.
+◦ time_to_sleep (in milliseconds): The time a philosopher will spend sleeping.
+◦ number_of_times_each_philosopher_must_eat (optional argument): If all
+philosophers have eaten at least number_of_times_each_philosopher_must_eat
+times, the simulation stops. If not specified, the simulation stops when a
+philosopher dies.
+• Each philosopher has a number ranging from 1 to number_of_philosophers.
+• Philosopher number 1 sits next to philosopher number number_of_philosophers.
+Any other philosopher number N sits between philosopher number N - 1 and philosopher number N + 1.
+ */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <pthread.h>
+# include <stdio.h>
+# include <limits.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <sys/time.h>
+
+/* Struct with argument values - setting the environment */
+typedef struct s_environment
+{
+	int	n_philo;
+	int	time_die;
+	int	time_eat;
+	int	time_sleep;
+	int	n_meals_opt;
+}	t_environment;
+
+
+#endif
