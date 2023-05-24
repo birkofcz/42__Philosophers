108e069604ea1b0af1381fcc8b32928181e1d42e
Stepan Benes <sbenes@c1r6s4.42prague.com>
2023-05-24 13:18:28 +0200

    args, error, init, Makefile

--- philo/init.c
+++ philo/init.c
@@ -6,7 +6,7 @@
 /*   By: sbenes <sbenes@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/24 10:25:22 by sbenes            #+#    #+#             */
-/*   Updated: 2023/05/24 12:41:06 by sbenes           ###   ########.fr       */
+/*   Updated: 2023/05/24 13:03:58 by sbenes           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,25 +41,25 @@ int	ft_arguments(int ac, char **av, t_environment *env)
 	if ((ft_isdigit(av[1])) == 0)
 		env->n_philo = ft_atoi(av[1]);
 	else
-		return (ft_error(1, "Bad argument"));
+		return (ft_error(1, "Bad arguments"));
 	if ((ft_isdigit(av[2])) == 0)
 		env->time_die = ft_atoi(av[2]);
 	else
-		return (ft_error(1, "Bad argument"));
+		return (ft_error(1, "Bad arguments"));
 	if ((ft_isdigit(av[3])) == 0)
 		env->time_eat = ft_atoi(av[3]);
 	else
-		return (ft_error(1, "Bad argument"));
+		return (ft_error(1, "Bad arguments"));
 	if ((ft_isdigit(av[4])) == 0)
 		env->time_sleep = ft_atoi(av[4]);
 	else
-		return (ft_error(1, "Bad argument"));
+		return (ft_error(1, "Bad arguments"));
 	if (ac == 6)
 	{
 		if ((ft_isdigit(av[5])) == 0)
 			env->n_meals_opt = ft_atoi(av[5]);
 		else
-			return (ft_error(1, "Bad argument"));
+			return (ft_error(1, "Bad arguments"));
 	}
 	return (0);
 }
--- philo/philosophers.c
+++ philo/philosophers.c
@@ -6,7 +6,7 @@
 /*   By: sbenes <sbenes@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/23 16:43:26 by sbenes            #+#    #+#             */
-/*   Updated: 2023/05/24 12:42:44 by sbenes           ###   ########.fr       */
+/*   Updated: 2023/05/24 13:04:19 by sbenes           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	main(int ac, char **av)
 	t_environment	env;
 
 	if (ac < 5 || ac > 6)
-		return (ft_error(1, "Not enough / Too much arguments"));
+		return (ft_error(1, "Bad arguments"));
 	ft_cleaninit(&env);
 	if (ft_arguments(ac, av, &env) != 0)
 		return (1);
